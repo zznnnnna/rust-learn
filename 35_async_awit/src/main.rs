452662dbd88cd73b48d1a426b2e0948ce6a3e3d4
zznnnnna <rex.zhao@cloudwise.com>
2022-09-20 18:40:40 +0800

    async

--- 35_async_awit/src/main.rs
+++ 35_async_awit/src/main.rs
@@ -1,3 +1,83 @@
+/*
+同步写法 基础
+ */
+
+// fn do3() {
+//     for i in 1..5 {
+//         println!("do3  {}", i);
+//     }
+// }
+//
+// fn do4() {
+//     for i in 1..5 {
+//         println!("do4  {}", i);
+//     }
+// }
+//
+// fn main() {
+//     do3();
+//     do4();
+// }
+
+
+/*
+异步写法01
+ */
+// use std::thread::{sleep, spawn};
+// use std::time::Duration;
+//
+// fn do3() {
+//     for i in 1..5 {
+//         println!("do3  {}", i);
+//         sleep(Duration::from_millis(500));
+//
+//     }
+// }
+//
+// fn do4() {
+//     for i in 1..5 {
+//         println!("do4  {}", i);
+//         sleep(Duration::from_millis(500));
+//
+//     }
+// }
+//
+// fn main() {
+//     let do3_spawn=spawn(do3);
+//     let do4_spawn=spawn(do4);
+//
+//     do3_spawn.join().unwrap();
+//     do4_spawn.join().unwrap();
+// }
+
+
+
+/*
+异步写法02
+ */
+use std::thread::{sleep, spawn};
+use std::time::Duration;
+
+fn do3() {
+    for i in 1..5 {
+        println!("do3  {}", i);
+        sleep(Duration::from_millis(500));
+
+    }
+}
+
+fn do4() {
+    for i in 1..5 {
+        println!("do4  {}", i);
+        sleep(Duration::from_millis(500));
+
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let do3_spawn=spawn(do3);
+    let do4_spawn=spawn(do4);
+
+    do3_spawn.join().unwrap();
+    do4_spawn.join().unwrap();
 }
