1337faa4c4d73230df762cdb60d739fddfe56443
n@windows <z@icods.cn>
2022-09-20 23:02:00 +0800

    实战

---
+++ project_demo/src/db.rs
@@ -0,0 +1,34 @@
+use crate::error_handler::CustomError;
+use diesel::pg::PgConnection;
+use diesel::r2d2::ConnectionManager;
+use lazy_static::lazy_static;
+use r2d2;
+use std::env;
+use diesel_migrations::embed_migrations;
+
+
+type Pool=r2d2::Pool<ConnectionManager<PgConnection>>;
+pub type DbConnection=r2d2::PooledConnection<ConnectionManager<PgConnection>>;
+
+embed_migrations!();
+
+lazy_static!{
+    static ref POOL:Pool={
+        let db_url=env::var("DATABASE_URL").expect("数据库url没设置");
+        let manager=ConnectionManager::<PgConnection>::new(db_url);
+        Pool::new(manager).expect("数据库连接池失败");
+    };
+}
+
+pub fn connection()->Result<DbConnection,CustomError> {
+    POOL.get().map_err(|e| CustomError::new(500,
+                                            format!("数据库连接失败：{}", e)
+    ))
+}
+
+pub fn init(){
+    lazy_static::initialize(&POOL);
+    let conn=connection().expect("获取数据库连接失败");
+    embedded_migrations::run(&conn).unwrap();
+
+}
---
+++ project_demo/src/main.rs
@@ -0,0 +1,8 @@
+mod error_handler;
+mod schema;
+mod db;
+mod employees;
+pub use employees::models;
+fn main() {
+    println!("Hello, world!");
+}
